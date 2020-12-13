aadc484a1555248a853af125f8901b91737af1dc
KangJiW00 <72329046+KangJiW00@users.noreply.github.com>
2020-12-13 16:05:39 +0900

    [URH-6][URH-7] 관심분야,언어 +버튼 누를시 박스 생성

--- js/mypage.js
+++ js/mypage.js
@@ -1,8 +1,28 @@
 window.onload = function() {
-    var clickcog = document.getElementById("cogwheel");
+    var cogwheel = document.getElementById("cogwheel");
     var passwdbox = document.getElementById("passwdbox");
+    var favoritefield = document.getElementById("favoritefield");
+    var resetbtn = document.getElementById("resetbtn");
+    var reflectbtn = document.getElementById("reflectbtn");
+    var combinebtns = document.getElementById("combinebtns");
+    var combinebtns2 = document.getElementById("combinebtns2");
+    var plusbtn1 = document.getElementById("plusbtn1");
+    var plusbtn2 = document.getElementById("plusbtn2");
 
-    clickcog.onclick = function() {
+    cogwheel.onclick = function() {
         passwdbox.style.display = "block";
     }
+    resetbtn.onclick = function() {
+        passwdbox.style.display = "none";
+    }
+    reflectbtn.onclick = function() {
+        passwdbox.style.display = "none";
+    }
+    plusbtn1.onclick = function() {
+        combinebtns.style.display = "block";
+    }
+    plusbtn2.onclick = function() {
+        combinebtns2.style.display = "block";
+    }
+
 };
