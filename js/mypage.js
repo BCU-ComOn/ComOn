ab72dc88dc4b8d4e79a0234b22481af6ff960d75
KangJiW00 <72329046+KangJiW00@users.noreply.github.com>
2020-12-13 13:50:18 +0900

    js파일 header참조 생성

--- js/mypage.js
+++ js/mypage.js
@@ -1,6 +1,3 @@
-document.getElementsByClassName("cogwheel").onclick = function() {
-    let cogwheel = document.getElementsByClassName("cogwheel");
-    cogwheel.onclick = function() {
-        document.getElementsByClassName("passwdbox").style.display='inline';
-    }
+window.onload = function() {
+    
 }
