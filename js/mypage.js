6afc0ac667884578bb6c019fea1c219a9c5435ef
KangJiW00 <72329046+KangJiW00@users.noreply.github.com>
2020-12-13 15:34:12 +0900

    [URH-35]톱니바퀴 눌렀을때 비밀번호 창 뜨는 js 작성완료

--- js/mypage.js
+++ js/mypage.js
@@ -1,3 +1,8 @@
 window.onload = function() {
-    
-}
+    var clickcog = document.getElementById("cogwheel");
+    var passwdbox = document.getElementById("passwdbox");
+
+    clickcog.onclick = function() {
+        passwdbox.style.display = "block";
+    }
+};
