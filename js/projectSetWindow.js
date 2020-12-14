36d417f255d81549a240cfab14db4099a8db77f7
escv12 <62186356+escv12@users.noreply.github.com>
2020-12-14 15:03:10 +0900

    [URH-17] 병합 안된거 수정

--- js/projectSetWindow.js
+++ js/projectSetWindow.js
@@ -3,18 +3,33 @@ window.onload = function() {
     let wrapper = document.getElementById("wrapper");
     let wrapper2 = document.getElementById("wrapper2");
 
-    let btnComp = document.getElementById("btnComp");
-    let btnContent = document.getElementById("btnContent");
+    let btnComp = document.getElementById("btnComp"); //기본정보
+    let btnContent = document.getElementById("btnContent"); //팀원관리
 
-    btnComp.onclick = function() {       
+    btnComp.onclick = function() {       //기본정보 클릭
         console.log(wrapper);
         wrapper.style.display = "block";
-        wrapper2.style.display = "none";  
+        wrapper2.style.display = "none";
+
+        
+        btnComp.style.background = "#309ce4";
+        btnComp.style.color = "#ffffff";
+        btnContent.style.backgroundColor = "#e0e0e0";
+        btnContent.style.color = "#333333";
+
+
     }
     
-    btnContent.onclick = function() {  
+    btnContent.onclick = function() {   //팀원관리 클릭
         console.log(wrapper2);
         wrapper.style.display = "none"; 
         wrapper2.style.display = "block"; 
+
+    
+        btnContent.style.backgroundColor = "#309ce4";
+        btnContent.style.color = "#ffffff";
+        btnComp.style.background = "#e0e0e0";
+        btnComp.style.color = "#333333";
+
     }
 }
