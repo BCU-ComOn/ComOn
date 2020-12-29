19f96f2485dcba60fb4c7afb3bfd2ce48fb16972
HGinsa <dlswo1225@gmail.com>
2020-12-29 14:24:19 +0900

    1229

---
+++ js/Introduction_Activity.js
@@ -0,0 +1,29 @@
+
+var mark = ["❕", "❔"]
+var subtitle = ["프론트엔드 기초과정", "프론트엔드 협업과정", "풀스택 체험기", "알고리즘 심층 분석", "다중 언어 체험기"]
+var righttext = ["HTML 어쩌구 저쩌구", "CSS 어쩌구 저쩌구", "HTML&CSS 활용", "JavaScript 기본", "웹 페이지 어쩌구 저쩌구"]
+
+
+document.write("<ul>");
+
+document.write("<li>");
+document.write("<strong><span>❕</span> 프론트엔드 기초과정</strong><br>");
+document.write("<div class = 'leftbox'>");
+document.write("<p>내는 것이 따뜻한 봄바람이다 인생에 따뜻한 봄바람을 불어 보내는 것은 청춘의 끓는 피다 청춘의 피가 뜨거운지라 인간의 동산에는 사랑의 풀이 돋고 이상의 꽃이 피고 희망의 놀이 뜨고 열락의 새가 운다사랑의 풀이 없으면</p>")
+document.write("<div id = 'orangebox'>");
+document.write("Community 내역 보기");
+document.write("</div>");
+document.write("</div>");
+document.write("<div class = 'rightbox'>");
+document.write("<ul>");
+document.write("<li>✔ HTML 어쩌구 저쩌구</li>");
+document.write("<li>✔ CSS 어쩌구 저쩌구</li>");
+document.write("<li>✔ HTML&CSS 활용</li>");
+document.write("<li>✔ JavaScript 기본</li>");
+document.write("<li>✔ 웹 페이지 어쩌구 저쩌구</li>");
+document.write("</ul>");
+document.write("</div>");
+document.write("</li>");
+document.write("<br>");
+
+document.write("</ul>");
