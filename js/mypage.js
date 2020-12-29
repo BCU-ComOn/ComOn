dc76f8dbdcffa3c523623682ead3f2b405893a92
kkg0726 <60912962+kkg0726@users.noreply.github.com>
2020-12-29 14:24:20 +0900

    URH-47 좋아오 누른 글 완성

--- js/mypage.js
+++ js/mypage.js
@@ -9,14 +9,13 @@ window.onload = function() {
     let btnContent = document.getElementById("btnContent");
     let btnLove = document.getElementById("btnLove");
 
-    //객체 만들어서 게시글 올리기
-    let article = {title: "제목1", created_at: "날짜1", name: "이름1", cont: "내용1"};
-    let article2 = {title: "제목2", created_at: "날짜2", name: "이름2", cont: "내용2"};
-    let article3 = {title: "제목2", created_at: "날짜2", name: "이름2", cont: "내용2"};
+    //내가 작성한 글
+    let mycontent = {title: "제목1", created_at: "날짜1", name: "이름1", cont: "내용1"};
+    let mycontent1 = {title: "제목2", created_at: "날짜2", name: "이름2", cont: "내용2"};
 
-    let articles = [article, article2, article3];
+    let mycontents = [mycontent, mycontent1];
 
-    for(var i = 0; i < articles.length; i++) {
+    for(var i = 0; i < mycontents.length; i++) {
         let cont_div = document.createElement("div");
         cont_div.classList.add("div");
         let cont_divHeader = document.createElement("div");
@@ -33,7 +32,6 @@ window.onload = function() {
         cont_divBody.classList.add("myContentBody");
         let cont_contImage = document.createElement("div");
         let cont_title = document.createElement("h2");
-        // cont_title.classList.add()
         let cont_cont = document.createElement("p");
 
         cont_div.appendChild(cont_divHeader);
@@ -46,12 +44,57 @@ window.onload = function() {
         cont_divBody.appendChild(cont_title);
         cont_divBody.appendChild(cont_cont);
 
-        cont_today.textContent = articles[i].created_at;
-        cont_name.textContent = articles[i].name;
-        cont_cont.textContent = articles[i].cont;
-        cont_title.textContent = articles[i].title;
+        cont_today.textContent = mycontents[i].created_at;
+        cont_name.textContent = mycontents[i].name;
+        cont_cont.textContent = mycontents[i].cont;
+        cont_title.textContent = mycontents[i].title;
 
         let content_list = document.getElementsByClassName("content-list")[0];
+        content_list.appendChild(cont_div);
+    }
+
+    //내가 작성한 글
+    let myLoveContent = {title: "제목1", created_at: "날짜1", name: "이름1", cont: "내용1"};
+    let myLoveContent1 = {title: "제목2", created_at: "날짜2", name: "이름2", cont: "내용2"};
+    let myLoveContent2 = {title: "제목3", created_at: "날짜3", name: "이름3", cont: "내용3"};
+
+    let myLoveContents = [myLoveContent, myLoveContent1, myLoveContent2];
+
+    for(var i = 0; i < myLoveContents.length; i++) {
+        let cont_div = document.createElement("div");
+        cont_div.classList.add("div");
+        let cont_divHeader = document.createElement("div");
+        cont_divHeader.classList.add("myContentHeader");
+        let cont_today = document.createElement("p");
+        cont_today.classList.add("today");
+        let cont_section = document.createElement("section");
+        cont_section.classList.add("right-align");
+        let cont_image = document.createElement("div");
+        cont_image.classList.add("myImage");
+        let cont_name = document.createElement("p");
+        cont_name.classList.add("myName");
+        let cont_divBody = document.createElement("div");
+        cont_divBody.classList.add("myContentBody");
+        let cont_contImage = document.createElement("div");
+        let cont_title = document.createElement("h2");
+        let cont_cont = document.createElement("p");
+
+        cont_div.appendChild(cont_divHeader);
+        cont_div.appendChild(cont_divBody);
+        cont_divHeader.appendChild(cont_today);
+        cont_divHeader.appendChild(cont_section);
+        cont_section.appendChild(cont_image);
+        cont_section.appendChild(cont_name);
+        cont_divBody.appendChild(cont_contImage);
+        cont_divBody.appendChild(cont_title);
+        cont_divBody.appendChild(cont_cont);
+
+        cont_today.textContent = myLoveContents[i].created_at;
+        cont_name.textContent = myLoveContents[i].name;
+        cont_cont.textContent = myLoveContents[i].cont;
+        cont_title.textContent = myLoveContents[i].title;
+
+        let content_list = document.getElementsByClassName("love-list")[0];
         content_list.appendChild(cont_div);
     }
 
