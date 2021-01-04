c2b0cecc59e19000aa3ba91d70cb9631ca82371d
LMY991 <dreamlmy1203@naver.com>
2021-01-04 14:54:04 +0900

    [BFS-130]버튼을 누르면 가입
    come on 영역을 누르면 페이지 변경에서 버그 발생
    ->
    오류 수정

--- apply/js/apply_comon page1.js
+++ apply/js/apply_comon page1.js
@@ -3,3 +3,16 @@ comeon.onclick=function() {
     var link = "apply_comon page2.html";
     location.href=link;
 }
+
+function handleOnChange(e){
+    //선택된 데이터 가져오기
+    const value=e.value;
+
+    //데이터 출력
+    let new_box=document.createElement('div');
+    new_box.className='interest';
+    new_box.textContent=value;
+
+    let content=document.getElementsByClassName('content')[0];
+    content.appendChild(new_box);
+}
